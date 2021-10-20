3e9590d0cb4bb5a01783820a76e1eee6796313c5
Markus Stetschnig <mstetschnig@entornosdeformacion.com>
2021-10-20 14:12:37 +0200

    Card example

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,15 +1,35 @@
 <template>
 <div class="card" style="width: 18rem;">
   <div class="card-body">
-    <h5 class="card-title">Card title</h5>
+    <h5 class="card-title">Titile Number<span v-if="num != 0">: {{num}}</span></h5>
     <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
     <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="#" class="card-link">Card link</a>
-    <a href="#" class="card-link">Another link</a>
+    <a v-on:click="countUp" class="card-link">Card link</a>
+    <a v-on:click="countDown" class="card-link">Another link</a>
   </div>
 </div>
 </template>
+
 <style>
-    /*@import 'https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css';*/
     @import '../../node_modules/bootstrap/dist/css/bootstrap.min.css';
 </style>
+
+<script>
+export default {
+  data () {
+    return {
+      num: 0
+    }
+  },
+  methods: {
+    countUp: function () {
+      this.num++;
+      console.log(this.num);
+    },
+    countDown: function () {
+      this.num--;
+      console.log(this.num);
+    }
+  }
+}
+</script>
