ed8a04b83077be6f24fc97f54b2e4dd6e3ceb3c0
Markus Stetschnig <mstetschnig@entornosdeformacion.com>
2021-10-20 13:25:29 +0200

    Avatart component prototype

---
+++ stories/Avatar.stories.js
@@ -0,0 +1,33 @@
+import Avatar from '../src/components/Avatar.vue';
+
+export default {
+  component: Avatar,
+  title: 'Components/Avatar',
+};
+
+const Template = (args, { argTypes }) => ({
+  components: { Avatar },
+  props: Object.keys(argTypes),
+  template: '<Avatar v-bind="$props"/>',
+});
+
+export const Primary = Template.bind({});
+Primary.args = {
+   square: false,
+   image: 'https://placekitten.com/500/500',
+   size: '200',
+};
+
+export const Square = Template.bind({});
+Square.args = {
+   square: true,
+   image: 'https://placekitten.com/400/400',
+   size: '200',
+};
+
+export const NoImage = Template.bind({});
+NoImage.args = {
+   square: true,
+   size: '200',
+   username: "My Name"
+};
