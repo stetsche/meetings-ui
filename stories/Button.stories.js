5ae2b1040c1695b45e50f85f641c5c3dacbd0292
Markus Stetschnig <mstetschnig@entornosdeformacion.com>
2021-10-27 13:20:45 +0200

    Remove Card, (old) Avatar and components and Change Button

--- stories/Button.stories.js
+++ stories/Button.stories.js
@@ -1,28 +1,16 @@
-import MyButton from '../src/components/Button.vue';
-
-// More on default export: https://storybook.js.org/docs/vue/writing-stories/introduction#default-export
+import SakaiButton from '../src/components/SakaiButton.vue';
 export default {
-  title: 'Archive/Button',
-  component: MyButton,
-  // More on argTypes: https://storybook.js.org/docs/vue/api/argtypes
-  argTypes: {
-    backgroundColor: { control: 'color' },
-    size: {
-      control: { type: 'select' },
-      options: ['small', 'medium', 'large'],
-    },
-  },
+  title: 'Components/SakaiButton',
+  component: SakaiButton,
 };
 
-// More on component templates: https://storybook.js.org/docs/vue/writing-stories/introduction#using-args
 const Template = (args, { argTypes }) => ({
   props: Object.keys(argTypes),
-  components: { MyButton },
-  template: '<my-button @onClick="onClick" v-bind="$props" />',
+  components: { SakaiButton },
+  template: '<sakai-button @onClick="onClick" v-bind="$props" />',
 });
 
 export const Primary = Template.bind({});
-// More on args: https://storybook.js.org/docs/vue/writing-stories/args
 Primary.args = {
   primary: true,
   label: 'Button',
@@ -30,17 +18,5 @@ Primary.args = {
 
 export const Secondary = Template.bind({});
 Secondary.args = {
-  label: 'Button',
-};
-
-export const Large = Template.bind({});
-Large.args = {
-  size: 'large',
-  label: 'Button',
-};
-
-export const Small = Template.bind({});
-Small.args = {
-  size: 'small',
   label: 'Button',
 };
