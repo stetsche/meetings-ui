2d3a017cb31fb17d8d17464b98718aa6623bf78b
Markus Stetschnig <mstetschnig@entornosdeformacion.com>
2021-10-29 15:06:59 +0200

    Avatar: Support text as an attribute

--- src/components/sakai-avatar.vue
+++ src/components/sakai-avatar.vue
@@ -1,17 +1,20 @@
 <template>
 <div class="avatar">
   <img
-    v-on:load="onLoad"
-    v-show="this.loaded" 
+    v-show="variant == 'image'" 
+    v-on:load="onImageLoad"
     :src="imageUrl"
     :width="size"
     :height="size"
     :style="imageStyle"
     :alt="altText"
   />
-  <div v-if="!this.loaded" v-bind:style="placeholderStyle">
+  <div v-if="variant == 'placeholder'" :style="placeholderStyle">
     <!-- This can be replaces with the icon component later -->
     <span class="fa fa-file-image-o"></span>
+  </div>
+  <div v-if="variant == 'text'" :style="placeholderStyle">
+    <span>{{text}}</span>
   </div>
 </div>
 </template>
@@ -33,17 +36,29 @@ export default {
       type: String, 
       default: 'circle',
     },
-    userId: { type: String, required: true },
-    userName: { type: String, required: true },
+    userId: { 
+      type: String, 
+    },
+    userName: { type: String, },
     siteId: { type: String, default: undefined },
     offical: { type: Boolean, default: false },
+    text: { type: String, default: undefined},
   },
   methods: {
-    onLoad: function () {
+    onImageLoad: function () {
       this.loaded = true;
     }
   },
   computed: {
+    variant: function () {
+      if(this.text) {
+        return 'text';
+      } else if (this.loaded) {
+        return 'image';
+      } else {
+        return 'placeholder';
+      }
+    },
     borderRadius: function () {
       return 'border-radius:' + (this.form == 'square'  ? this.sqareRadius : '50%') + ';';
     }, 
@@ -54,6 +69,15 @@ export default {
       }
       return style;
     },
+    fontSize: function () {
+      if(this.variant == 'placeholder') {
+        return this.size / 2.5; 
+      } else if (this.text.length <= 3){
+        return this.size / 2;
+      } else {
+        return this.size / (this.text.length * (2/3));
+      }
+    },
     placeholderStyle: function () {
       var style = this.borderRadius;
       style += 'background-color:' + this.backgroundColor + ';';
@@ -61,8 +85,7 @@ export default {
       style += 'width:' + this.size +'px;';
       style += 'height:' + this.size +'px;';
       style += 'display: flex;flex-direction: column;justify-content: center; text-align: center;';
-      style += 'font-size:' + this.size / 2.5  + 'px;';
-      console.log(style);
+      style += 'font-size:' + this.fontSize + 'px;';
       return style;
     },
     imageUrl: function () {
--- stories/sakai-avatar.stories.js
+++ stories/sakai-avatar.stories.js
@@ -5,6 +5,7 @@ export default {
   title: 'Ready/sakai-avatar',
   argTypes: {
     userId: { control: "select", options: [
+      '',
       '454db719-443a-400f-b4d4-4dfada8091c0', 
       '67aefef6-32df-8fe7-87fe-90721ar79def', 
       '34058dsf-fd23-w9df-9ed6-234dsf381edn',
@@ -22,30 +23,37 @@ const Template = (args, { argTypes }) => ({
 
 export const Primary = Template.bind({});
 Primary.args = {
-   size: 300,
-   userId: "67aefef6-32df-8fe7-87fe-90721ar79def",
-   userName: "Aufderhar Jamison",
+  size: 300,
+  userId: "67aefef6-32df-8fe7-87fe-90721ar79def",
+  userName: "Aufderhar Jamison",
 };
 
 export const NoImage = Template.bind({});
 NoImage.args = {
-   size: 300,
-   userId: '454db719-443a-400f-b4d4-4dfada8091c0',
-   userName: 'Victor van Dijk'
+  size: 300,
+  userId: '454db719-443a-400f-b4d4-4dfada8091c0',
+  userName: 'Victor van Dijk'
 };
 
 export const Square = Template.bind({});
 Square.args = {
-   form: 'square',
-   size: 300,
-   userId: '9072hbs3-sb23-sfef-f93r-9q678g7g3qrh',
-   userName: 'Bailey Ruthe'
+  form: 'square',
+  size: 300,
+  userId: '9072hbs3-sb23-sfef-f93r-9q678g7g3qrh',
+  userName: 'Bailey Ruthe'
 };
 
 export const Broken = Template.bind({});
 Broken.args = {
-   form: 'square',
-   size: 300,
-   userId: 'this-is-not-a-user-id',
-   userName: 'no one'
+  form: 'square',
+  size: 300,
+  userId: 'this-is-not-a-user-id',
+  userName: 'no one'
 };
+
+export const Text = Template.bind({});
+Text.args = {
+  size: 300,
+  text: '+6',
+};
+  color: 'green'
