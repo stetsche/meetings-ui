b93d3becc41eceb174cd931e3711352900ce8b20
Markus Stetschnig <mstetschnig@entornosdeformacion.com>
2021-11-23 17:56:29 +0100

    Add icon component

---
+++ src/assets/icons.js
@@ -0,0 +1,13 @@
+export default {
+  file_image: "fa fa-file-image-o",
+  hourglass_emty: "fa fa-hourglass-o",
+  bell: "fa fa-bell",
+  error: "fa fa-ban",
+  live: "bi bi-record-circle livered",
+  chat: "bi bi-chat-right-text",
+  presentation: "bi bi-easel",
+  play: "bi bi-play-circle",
+  attachment: "bi bi-paperclip",
+  vidocamera: "fa fa-video-camera",
+  menu_kebab: "fa fa-ellipsis-v",
+};
---
+++ src/components/sakai-icon.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="sakai-icon" aria-hidden="true">
+    <i :class="iconClass"></i>
+  </div>
+</template>
+
+<script>
+import icons from "../assets/icons.js";
+
+export default {
+  props: {
+    iconkey: {
+      Type: String,
+      default: "error",
+      validator: function (value) {
+        return Object.entries(icons).indexOf(value) > -1;
+      },
+    },
+  },
+  computed: {
+    iconClass: function () {
+      console.log(icons["live"]);
+      return icons[this.iconkey];
+    },
+  },
+};
+</script>
